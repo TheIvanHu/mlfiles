0481b2526962aba7cafe594db74d0d40f4b0a5d4
Ivan Hu <ivanhu04@gmail.com>
2021-09-18 20:12:41 -0400

    files

---
+++ summary.py
@@ -0,0 +1,49 @@
+from punctuator import Punctuator
+import json
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize, sent_tokenize
+import bs4 as BeautifulSoup
+import urllib.request  
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import re
+def summarize(subtitles):
+
+    text = []
+    for i in subtitles:
+        text.append(i['text'])
+    text = " ".join(text)
+
+    #getting rid of contractions
+    contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is", "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock",  "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are", "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have", "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have"}
+    def preprocessing(text):
+        #gets rid of contractions
+        text = text.lower() 
+        text = text.split() 
+        for i in range(len(text)):
+            word = text[i]
+            if word in contraction_mapping:
+                text[i] = contraction_mapping[word]
+        text = " ".join(text)
+        text = re.sub(r'[^a-zA-Z0-9. ]','',text) # remove punctuations
+        text = re.sub(r'\.',' . ',text)
+                
+        return text
+
+    text = preprocessing(text)
+    p = Punctuator('INTERSPEECH-T-BRNN.pcl')
+    text= p.punctuate(text)
+
+    #print(transcript)
+    model_name = 'google/pegasus-large'
+
+
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name)
+        
+    batch = tokenizer(text, truncation=True, padding='longest', return_tensors="pt")
+    translated = model.generate(**batch)
+    summary_results = tokenizer.batch_decode(translated, skip_special_tokens=True)
+
+    return(summary_results)
+
